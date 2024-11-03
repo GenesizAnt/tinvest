b08338a18ac38072361f7ccf63a737189b7d9312
Anton <genesiz1990@gmail.com>
2024-11-03 23:18:05 +0300

    init commit

---
+++ src/main/java/com/investinfo/capital/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.investinfo.capital;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CapitalApplication.class);
+	}
+
+}
